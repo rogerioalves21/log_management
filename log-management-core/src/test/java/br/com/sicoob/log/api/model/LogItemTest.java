e0381b408e737f448fd9858982c79e5ea92d509a
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-09-23 02:51:20 +0000

    Primeiros testes

---
+++ log-management-core/src/test/java/br/com/sicoob/log/api/model/LogItemTest.java
@@ -0,0 +1,24 @@
+package br.com.sicoob.log.api.model;
+
+import org.junit.*;
+
+/**
+ * Testes para o log item.
+ * @author Rogerio Alves Rodrigues
+ */
+public class LogItemTest {
+  
+  /**
+   * Testa os atributos.
+   */
+  @Test
+  public void devePreencherLogItem() {
+    LogItem log = new LogItem();
+    log.setType(LogType.INFO);
+    log.setMessage("Mensagem de teste");
+    
+    Assert.assertNotNull(log.getType());
+    Assert.assertNotNull(log.getMessage());
+  }
+  
+}
