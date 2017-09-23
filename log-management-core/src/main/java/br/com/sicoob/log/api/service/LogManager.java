0da5504afa1c86c18a9335375b544760236a1e65
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-09-23 03:41:13 +0000

    Opa

---
+++ log-management-core/src/main/java/br/com/sicoob/log/api/service/LogManager.java
@@ -0,0 +1,36 @@
+package br.com.sicoob.log.api.service;
+
+import br.com.sicoob.log.api.model.LogItem;
+
+/**
+ * Classe para chamar o log.
+ * @author Rogerio Alves Rodrigues
+ */
+public final class LogManager {
+
+    private static final LogManager INSTANCE = new LogManager();
+
+    /**
+     * Construtor privado.
+     */
+    private LogManager() {
+
+    }
+
+    /**
+     * Obtem a instancia de LogManager.
+     */
+    public static LogManager getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Realiza o log da mensagem.
+     * @param item Log.
+     */
+    public void log(LogItem item) {
+        ServiceApi api = new ServiceApiImpl();
+        api.log(item);
+    }
+
+}
