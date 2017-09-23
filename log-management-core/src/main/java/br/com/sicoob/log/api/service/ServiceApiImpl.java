dd1c634ec04b1aaddb2a4d0121bb0071ce27b51d
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-09-23 03:30:32 +0000

    Cliente

--- log-management-core/src/main/java/br/com/sicoob/log/api/service/ServiceApiImpl.java
+++ log-management-core/src/main/java/br/com/sicoob/log/api/service/ServiceApiImpl.java
@@ -1,21 +1,31 @@
 package br.com.sicoob.log.api.service;
 
 import br.com.sicoob.log.api.model.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
 
 /**
  * Implementacao do acesso a api de log.
  * @author Rogerio Alves Rodrigues
  */
 public class ServiceApiImpl implements ServiceApi {
-  
-  private static final String PROVIDER = "http://localhost:8080/log";
-  private static final String PATH = "api/log";
-  
-  /**
-   * 
-   */
-  public void log(LogItem log) {
-    
-  }
-  
+
+    private static final String PROVIDER = "http://localhost:8080/log";
+    private static final String PATH     = "api/log";
+
+    /**
+     * 
+     */
+    public void log(LogItem log) {
+        Client client = ClientBuilder.newClient();
+        client.register(MoxyJsonFeature.class);
+        WebTarget target = client.target(PROVIDER).path(PATH);
+        target.request().post(Entity.entity(log, MediaType.APPLICATION_JSON_TYPE));
+    }
+
 }
