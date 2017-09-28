f9b33c1594c075407e6eb4fdb9f695b338d79f13
Rogerio Alves Rodrigues <rogerioalves21@gmail.com>
2017-09-28 13:31:46 +0000

    Listar

--- log-management-web/src/main/java/br/com/sicoob/log/dao/LogManagementDao.java
+++ log-management-web/src/main/java/br/com/sicoob/log/dao/LogManagementDao.java
@@ -2,16 +2,13 @@ package br.com.sicoob.log.dao;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.NavigableSet;
+import java.util.*;
 
 import org.mapdb.DB;
 import org.mapdb.DB.HashMapMaker;
 import org.mapdb.DBMaker;
 import org.mapdb.HTreeMap;
 
-import br.com.sicoob.log.api.model.LogItem;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Acesso a base de dados.
@@ -21,18 +18,27 @@ import java.util.logging.Logger;
 public class LogManagementDao {
 
     /**
+     * Obtem a lista de logs salvos em memoria.
+     * 
+     * @return Lista de logs.
+     * @throws Erro ao acessar o banco em memoria.
+     */
+    public List listar() throws IOException {
+        File dbFile = File.createTempFile("mapdb", "db");
+        DB db = DBMaker.fileDB(dbFile).closeOnJvmShutdown().make();
+        NavigableSet treeSet = db.treeSet("logs").createOrOpen();
+    }
+
+    /**
      * Salva em memoria o item de log.
      * 
      * @param item Log.
-     * @throws Erro ao salvar o item.
      * @throws Erro ao criar o arquivo de log.
      */
     public void salvar(LogItem item) throws IOException {
-        Logger.getLogger(getClass().getName()).log(Level.INFO, "Erro ao salvar um item");
         File dbFile = File.createTempFile("mapdb", "db");
         DB db = DBMaker.fileDB(dbFile).closeOnJvmShutdown().make();
-
-        NavigableSet treeSet = db.treeSet("example").createOrOpen();
+        NavigableSet treeSet = db.treeSet("logs").createOrOpen();
         treeSet.add(item);
     }
 
